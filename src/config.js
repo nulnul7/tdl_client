119f66a63ef8b8d773f2c523c01f306f481f88e1
nulnul7 <57282453+nulnul7@users.noreply.github.com>
2022-12-21 17:17:45 +0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create({
-    baseURL: 'http://localhost:5501/5R2I/todo/'
+    baseURL: 'https://odd-pink-newt-gear.cyclic.app/5R2I/todo/'
 })
